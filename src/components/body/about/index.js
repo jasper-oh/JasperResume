590439fa3d5789e0c770a40c890933cf1f4e9698
jasper <63331153+jasper-oh@users.noreply.github.com>
2021-07-07 00:32:36 +0900

    edit

--- src/components/body/about/index.js
+++ src/components/body/about/index.js
@@ -7,7 +7,7 @@ function About() {
       <div className="about-top">
         <div className="about-info">
           {/* 배치수정 ====%%%% */}
-          안녕하세요 👋, 백엔드 개발자 <br />
+          안녕하세요 👋, 개발자 <br />
           <span className="info-name">오영준 Jasper</span> 입니다!
           <br /> 분야를 가리지 않고, 스펀지 같은 흡수력으로 <br />
           끊임없이 성장중입니다 !
