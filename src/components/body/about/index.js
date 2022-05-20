ce82e591a39b3a75e75dafeb24565e73e65e4332
jasper <63331153+jasper-oh@users.noreply.github.com>
2022-05-21 00:34:40 +0900

    Translate it  to English

--- src/components/body/about/index.js
+++ src/components/body/about/index.js
@@ -10,7 +10,7 @@ function About() {
           Hello 👋, I am a developer <br />
           <span className="info-name">Jasper</span>
           <br /> constantly growing with <br />
-          absorbing every field.
+          absorbing every kind of field.
         </div>
         <div className="about-photo">
           <img
