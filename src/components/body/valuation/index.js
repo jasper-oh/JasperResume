e08e2d0358cd00d756a865247a02f70d4792c5f0
Jasper Oh <ohyj0906@gmail.com>
2023-08-09 23:51:20 +0900

    [Add] Add design of tooltop and put data

--- src/components/body/valuation/index.js
+++ src/components/body/valuation/index.js
@@ -12,13 +12,22 @@ import {
 } from 'recharts';
 import "./valuation.css";
 import Separator from "../../common/separator";
+import { ValuationData } from "../../../data/valuation";
+
+const getDescription = (numOfData) => {
+  return ValuationData[numOfData-1].description
+}
+
 
 const CustomTooltip = ({active, payload, label }) => {
+
   if(active && payload && payload.length){
+    let title = payload[0].payload.title;  
+    let numOfData = payload[0].payload.no;
     return (
       <div className="custom-tooltip">
-        {/* <p className="label">{`${label} : ${payload[0].value}`}</p> */}
-        <p className="intro">{label}</p>
+        <p className="label">{`${title}`}</p>
+        {getDescription(numOfData)}
         <p className="desc"></p>
       </div>
     )
@@ -30,44 +39,60 @@ function Valuation() {
 
   const data = [
     {
-      name: '20s',      
+      no : 1,
+      name: '20s',     
+      title: '20s', 
       valuation: 10,
       amt: 2400,
     },
     {
-      name: '',      
+      no : 2,
+      name: '',    
+      title: "Customer Service",  
       valuation: 20,
       amt: 2210,
     },
     {
+      no : 3,
       name: '',      
+      title: "Boot Camp",  
       valuation: 15,
       amt: 2290,
     },
     {
-      name: '',      
+      no : 4,
+      name: '', 
+      title: "Start Working",      
       valuation: 25,
       amt: 2000,
     },
     {
-      name: 'BCIT',      
+      no : 5,
+      name: 'BCIT',  
+      title: "BCIT",      
       valuation: 30,
       amt: 2000,
     },
     {
+      no : 6,
       name: 'Now',
+      title: "Now",      
       prediciton: 40,
       valuation: 40,
       amt: 2181,
     },
     {
+      no : 7,
       name: '',
+      title : "During Co-op",
       prediciton: 45,      
       amt: 2500,
     },
   
     {
+      no : 8,
       name: '30s',
+      title: "After Graduation",      
       prediciton: 60,      
       amt: 2100,
     },
@@ -83,8 +108,8 @@ function Valuation() {
           <LineChart width={400} height={300} data={data}>          
             <XAxis dataKey="name" padding={{ left: 30, right: 30 }} />          
             <ReferenceLine x="Now" label="Now"/>
-            <Line type="monotone" dataKey="valuation" stroke="#8884d8" activeDot={{ r: 8 }}/>          
-            <Line type="monotone" dataKey="prediciton" stroke="#8884d8" strokeDasharray="4 1"/>          
+            <Line type="monotone" dataKey="valuation" stroke="#1a48a0" strokeWidth={2} activeDot={{ r: 8 }}/>          
+            <Line type="monotone" dataKey="prediciton" stroke="#1a48a0" strokeWidth={2} strokeDasharray="4 1"/>          
             <Tooltip content={<CustomTooltip/>} position={{x: 0, y:300}}/>          
           </LineChart>        
         </ResponsiveContainer>
---
+++ src/data/valuation.js
@@ -0,0 +1,72 @@
+export const ValuationData = [
+  {    
+    description : 
+      <div className="valuation-desc-box"> 
+        <p className="intro">[something] Start learning Computer Science in University</p>
+        <p className="intro">[something] Learn Basic C, Java, Computer Architecture</p>
+      </div>
+  },
+  {    
+    description : 
+      <div className="valuation-desc-box"> 
+        <p className="intro">[something] Successfully finished military service as a squad leader</p>
+        <p className="intro">[something] Back to University and learn Data Structure, Network System</p>
+        <p className="intro">[something] Drop out University for navigating my career</p>
+        <p className="intro">[something] Work in Samsung Everland (largest theme park in Korea) as a Customer Service Specialist</p>
+        <p className="intro">[something] Work in CGV (largest movie theatre in Korea) as a Customer Service</p>
+      </div>
+  },
+  {    
+    description : 
+      <div className="valuation-desc-box"> 
+        <p className="intro">[something] After finished customer service career, Join to BootCamp for being a programmer</p>
+        <p className="intro">[something] Learn Java and Build Swing (Java GUI) Project</p>
+        <p className="intro">[something] Learn Basic Web System with JSP (Java Servlet Page) Project</p>
+        <p className="intro">[something] Learn Basic Android System with Java and build Android App Project</p>
+        <p className="intro">[something] Learn Basic IOS with Swift and build IOS app project with Swift and registered in App Store</p>
+        <p className="intro">[something] Start getting personal interest in web project and Start SpringBoot project with Java by myself</p>
+      </div>
+  },
+  {    
+    description : 
+      <div className="valuation-desc-box"> 
+        <p className="intro">[something] Join System Integration Company located in Korea as a Junior Backend Developer position</p>
+        <p className="intro">[something] Dispatch to System advancement project that provided by Shinhan bank</p>
+        <p className="intro">[something] Advance the Legacy Java Project by using Spring Boot and Thymeleaf</p>
+        <p className="intro">[something] Support Senior Engineer, Documented Development Process with the provided guideline</p>
+        <p className="intro">[something] …</p>
+        <p className="intro">[something] …</p>
+      </div>
+  },
+  {    
+    description : 
+      <div className="valuation-desc-box"> 
+        <p className="intro">[something] To get more experience and broaden my horizon, Join to Computer System Technology in BCIT</p>
+        <p className="intro">[something] Participate in the Hackathon named Hack the Break as a team member, build a resume check, Job search web project</p>
+        <p className="intro">[something] Successfully finished web project named “Howdy Weather”, which using HTML, CSS, NodeJS, google firebase as a team lead (3 members)</p>
+        <p className="intro">[something] Successfully finished web project named “SAM - Stories A Million” using NextJS and Prisma ORM, MongoDB as a Database as a team lead (4 members)</p>
+        <p className="intro">[something] Participate in the Hackathon named Hackrx2023 as a team member, build a web service, which track the patient by pharmacist</p>
+      </div>
+  },
+  {    
+    description : 
+      <div className="valuation-desc-box"> 
+        <p className="intro">[something] Start Personal project with Spring Boot to developing the skills</p>
+        <p className="intro">[something] Start learning Data Structure and algorithms for more advanced skills </p>
+      </div>
+  },
+  {    
+    description : 
+      <div className="valuation-desc-box"> 
+        <p className="intro">[something] Start learning Computer Science in University</p>
+        <p className="intro">[something] Learn Basic C, Java, Computer Architecture</p>
+      </div>
+  },
+  {    
+    description : 
+      <div className="valuation-desc-box"> 
+        <p className="intro">[something] Start Personal project with Spring Boot to developing the skills</p>
+        <p className="intro">[something] Start learning Data Structure and algorithms for more advanced skills </p>
+      </div>
+  }
+]
