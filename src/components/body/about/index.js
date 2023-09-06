3f365f77b8764be888ac0a8e5e9f00aa1a2dfe7b
Jasper Oh <ohyj0906@gmail.com>
2023-09-06 09:12:19 -0700

    [Edit] Cover Photo

--- src/components/body/about/index.js
+++ src/components/body/about/index.js
@@ -14,7 +14,7 @@ function About() {
         </div>
         <div className="about-photo">
           <img
-            src={require("../../../assets/image/coding4.png").default}
+            src={require("../../../assets/image/coding5.jpg").default}
             className="picture"
             id="picturething"
           />
